3721ad5d71bcf0c499d6b1d58167aacebeca96e4
sadman saqib <62344899+sadman9494@users.noreply.github.com>
2022-02-02 09:26:21 +0600

    Add files via upload

---
+++ Task1.php
@@ -0,0 +1,53 @@
+<html>
+    
+<body>
+<h3> Ragistration Form</h3>
+    
+    <form action = "/action_page.php">
+    <table> 
+        <tr>
+           <td> First Name : </td>
+            <td><input type ="text" fname = "firstname" > </td>
+        </tr>
+        <tr>
+            <td>Last name :</td>
+            <td><input type = "text" lname = "lastname"></td>
+        </tr>
+        <tr>
+            <td>Age:</td>
+            <td><input type = "number" age = "age" min ="1" max ="100"></td>
+        </tr>
+        <tr>
+            <td>Designation :</td>
+            <td>
+            <input type = "radio" name = "designation" > Junior Programmer 
+             <input type = "radio" name = "designation" > Senior Programmer
+             <input type = "radio" name = "designation" > Project Lead
+            </td> 
+        <tr>
+            <td>Preffered Language :</td> 
+            <td><input type = "checkbox" name = "language1" value = "Java"> 
+            <input type = "checkbox" name = "language1" value = "PHP"> 
+            <td><input type = "checkbox" name = "language1" value = "C++"></td> 
+        </tr>
+        <tr>
+            <td>First Eamil :</td> 
+            <td><input type ="text" email = "email" ></td>  
+        </tr>
+        <tr>
+        <td>Password :</td> 
+        <td><input type ="password" password = "password"  ></td> 
+        </tr>
+        <tr>  
+        <td>Please choose a file :</td>  
+        <td><input type = "file" file = "file"> </td> 
+        </tr>
+        <tr>
+        <td><input type = "submit"> </td> 
+        <td><input type = "reset"></td> 
+        </tr>
+    </table>
+ </from>
+
+    </body>
+</html?
